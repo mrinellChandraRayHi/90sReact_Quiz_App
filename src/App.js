b63b1462030af8bb9f47c55d79d525dfbd15ef90
MrinellChandraRay <mrinellroy@gmail.com>
2024-03-18 00:45:16 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Question from "./Components/Question";
+import qBank from "./Components/QuestionBank";
+import Score from "./Components/Score";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const [questionBank] = useState(qBank);
+	const [currentQuestion, setCurrentQuestion] = useState(0);
+	const [selectedOption, setSelectedOption] = useState("");
+	const [score, setScore] = useState(0);
+	const [quizEnd, setQuizEnd] = useState(false);
+
+	const handleOptionChange = (e) => {
+		setSelectedOption(e.target.value);
+	};
+
+	const handleFormSubmit = (e) => {
+		e.preventDefault();
+		checkAnswer();
+		handleNextQuestion();
+	};
+
+	const checkAnswer = () => {
+		if (selectedOption === questionBank[currentQuestion].answer) {
+		setScore((prevScore) => prevScore + 1);
+		}
+	};
+
+	const handleNextQuestion = () => {
+		if (currentQuestion + 1 < questionBank.length) {
+		setCurrentQuestion((prevQuestion) => prevQuestion + 1);
+		setSelectedOption("");
+		} else {
+		setQuizEnd(true);
+		}
+	};
+
+	return (
+		<div className="App d-flex flex-column align-items-center justify-content-center">
+		<h1 className="app-title">QUIZ APP</h1>
+		{!quizEnd ? (
+			<Question
+			question={questionBank[currentQuestion]}
+			selectedOption={selectedOption}
+			onOptionChange={handleOptionChange}
+			onSubmit={handleFormSubmit}
+			/>
+		) : (
+			<Score score={score} onNextQuestion={handleNextQuestion} className="score" />
+		)}
+		</div>
+	);
+};
 
 export default App;
